054fb356647535d9b94e50264ed91fc39a597577
Kashyap <kashyap.mistry.btech2021@sitpune.edu.in>
2024-09-30 16:53:04 +0530

    commit3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import MovieDetailsPage from './pages/MovieDetailsPage';
+import MovieDetailsPage from './pages/MovieDetailPage';
 import Header from './components/Header';
 
 const App = () => {
