a4f94d1c066c5c19c222cff3fb1fde4d07608310
Kashyap <kashyap.mistry.btech2021@sitpune.edu.in>
2024-09-30 14:36:45 +0530

    commit2

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+return (
+    <header className="header">
+    <h1>MultiFlix</h1>
+    <nav>
+        <Link to="/">Home</Link>
+    </nav>
+    </header>
+);
+};
+
+export default Header;
